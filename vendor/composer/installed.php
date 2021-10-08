9ec6778cfa45be0e4f0f3a191d4884805ebb5318
soysauce <xdqamy@163.com>
2021-10-08 17:17:47 +0800

    composer-commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '328532487a068d7477bef5a9604a45914c8c3b02',
+    'name' => 'object/dada-api',
+  ),
+  'versions' => 
+  array (
+    'easyswoole/http-client' => 
+    array (
+      'pretty_version' => '1.5.0',
+      'version' => '1.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6f1f247495983d0494465851c263f2b2c893f8bb',
+    ),
+    'easyswoole/spl' => 
+    array (
+      'pretty_version' => '1.4.0',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5549de955cee82a9c7edd8ea34d4df4d14b51b38',
+    ),
+    'object/dada-api' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '328532487a068d7477bef5a9604a45914c8c3b02',
+    ),
+  ),
+);
